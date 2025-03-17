0252d57a62c07486734016c5bdd80e9a435f323a
lowiqxd <loic.aviez91@gmail.com>
2025-03-17 10:52:58 +0100

    Ajout des routes + création du composant footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import type {Routes} from '@angular/router';
+import {AboutPageComponent} from './pages/about-page/about-page.component';
 import {HomePageComponent} from './pages/home-page/home-page.component';
 import {NotFoundPageComponent} from './pages/not-found-page/not-found-page.component';
+import {ProjectsPageComponent} from './pages/projects-page/projects-page.component';
 
 export const routes: Routes = [
-  { path: '', component: HomePageComponent },
+  { path: '', component: HomePageComponent }, // Home
+  { path: 'projects', component: ProjectsPageComponent }, // Projects
+  { path: 'about', component: AboutPageComponent }, // About
   { path: '**', component: NotFoundPageComponent }, // 404
 ];

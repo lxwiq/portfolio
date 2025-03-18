6e5c8fd0fca9d3b380e1cb498bbcfee3ee9238f6
Getloloed <loic.aviez91@gmail.com>
2025-03-18 11:59:49 +0100

    Ajoute du contenu et des fonctionnalités à la page "About"
    Ajout d'un paragraphe supplémentaire et d'une image à la page "About". Introduction d'une interface "Stack" et ajout d'une liste de stacks (Angular, Ionic, etc.) dans le composant TypeScript. Mise à jour du style pour gérer l'image nouvellement ajoutée.

--- src/app/pages/about-page/about-page.component.ts
+++ src/app/pages/about-page/about-page.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../features/components/footer/footer.component';
 
+interface Stack {
+  logo: string | SVGElement;
+  name: string;
+}
+
 @Component({
   selector: 'app-about-page',
   standalone: true,
@@ -8,4 +13,23 @@ import { FooterComponent } from '../../features/components/footer/footer.compone
   templateUrl: './about-page.component.html',
   styleUrl: './about-page.component.scss',
 })
-export class AboutPageComponent {}
+export class AboutPageComponent {
+  stacks = [
+    {
+      logo: 'angular',
+      name: 'Angular',
+    },
+    {
+      logo: 'ionic',
+      name: 'Ionic',
+    },
+    {
+      logo: 'capacitor',
+      name: 'Capacitor',
+    },
+    {
+      logo: 'firebase',
+      name: 'Firebase',
+    },
+  ];
+}

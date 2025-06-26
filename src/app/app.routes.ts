459dbf4bbc6bceda4622a10a053a4ddc5d2c0904
Loic <loic.aviez91@gmail.com>
2025-06-26 11:03:02 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,3 @@
-import type { Routes } from '@angular/router';
-import { AboutPageComponent } from './pages/about-page/about-page.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
-import { ProjectsPageComponent } from './pages/projects-page/projects-page.component';
+import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-  { path: '', component: HomePageComponent }, // Home
-  { path: 'projects', component: ProjectsPageComponent }, // Projects
-  { path: 'about', component: AboutPageComponent }, // About
-  { path: '**', component: NotFoundPageComponent }, // 404
-];
+export const routes: Routes = [];
